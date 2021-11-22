9591cb068f39775ee198d49e814f421e4f7ffd98
GT ɮɛʀռօ <78096979+GTBERNO@users.noreply.github.com>
2021-11-22 11:01:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class Config(object):
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "")
     MONGO_STR = environ.get("MONGO_STR", "")
-    CHANNELID = environ.get("CHANNELID", "")
-    GROUPID = environ.get("GROUPID", "")
+    #CHANNELID = environ.get("CHANNELID", "")
+    #GROUPID = environ.get("GROUPID", "")
 
